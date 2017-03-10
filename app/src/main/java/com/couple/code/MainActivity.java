24b1cc0f8cb6d41d87510e7fd125ce0edcc6dc0a
Deepsilence_tcc <490913105@qq.com>
2017-03-10 14:32:55 +0800

    butterknife

--- app/src/main/java/com/couple/code/MainActivity.java
+++ app/src/main/java/com/couple/code/MainActivity.java
@@ -1,15 +1,39 @@
 package com.couple.code;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.home_menu)
+    ImageView homeMenu;
+    @BindView(R.id.btn_calendar)
+    Button btnCalendar;
+    @BindView(R.id.btn_calculator)
+    Button btnCalculator;
+    @BindView(R.id.btn_weather)
+    Button btnWeather;
+    @BindView(R.id.btn_express)
+    Button btnExpress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
+        homeMenu.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this,"点击我了",Toast.LENGTH_SHORT).show();
     }
 }
