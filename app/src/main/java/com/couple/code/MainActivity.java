08cdcd43c432fae4ef9e476ffbdf061c4847f3be
Deepsilence_tcc <490913105@qq.com>
2017-03-10 15:28:24 +0800

    basework

--- app/src/main/java/com/couple/code/MainActivity.java
+++ app/src/main/java/com/couple/code/MainActivity.java
@@ -1,11 +1,11 @@
 package com.couple.code;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -29,11 +29,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        homeMenu.setOnClickListener(this);
+        btnCalculator.setOnClickListener(this);
+        btnCalendar.setOnClickListener(this);
+        btnWeather.setOnClickListener(this);
+        btnExpress.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
-        Toast.makeText(this,"点击我了",Toast.LENGTH_SHORT).show();
+        Intent intent = null;
+
+        switch (v.getId()){
+            case R.id.btn_calendar:
+
+                intent = new Intent(this,CalendarActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btn_express:
+                intent = new Intent(this,ExpressActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
